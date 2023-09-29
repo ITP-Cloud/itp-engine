d1eceff2ac5f8af2b269593ead77b1ef267fe11a
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-09-29 11:29:53 +0300

    update: added springboot

---
+++ src/main/java/org/itp/engine/ITPEngineApplication.java
@@ -0,0 +1,12 @@
+package org.itp.engine;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITPEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ITPEngineApplication.class, args);
+    }
+}

0601ec32c3ee8a5aa2bb7af6e9e60a29345ad7f9
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-09-30 00:15:23 +0300

    update: added endpoint to create a user & cors

--- src/main/java/org/itp/engine/useraccount/UserAccountController.java
+++ src/main/java/org/itp/engine/useraccount/UserAccountController.java
@@ -1,13 +1,32 @@
 package org.itp.engine.useraccount;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.itp.engine.restful.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 public class UserAccountController {
 
-    @GetMapping("api/v1/lx-usr")
-    public String getLinuxUsers(){
-        return "Some users";
+    private final UserAccountService service;
+
+    @Autowired
+    public UserAccountController(UserAccountService service){
+        this.service = service;
+    }
+
+    @PostMapping("api/v1/user-account")
+    public ResponseEntity<ApiResponse> createUser(@RequestBody UserAccount request){
+
+//        this.service.createUser(request);
+
+        ApiResponse response = new ApiResponse(
+                "User account successfully created",
+                HttpStatus.OK.value()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }

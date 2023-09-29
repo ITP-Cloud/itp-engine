a27fc65ea58ffc29656ae1748c8e0e821cb5107f
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-09-30 00:10:31 +0300

    update: add create user method in service

--- src/main/java/org/itp/engine/useraccount/UserAccountService.java
+++ src/main/java/org/itp/engine/useraccount/UserAccountService.java
@@ -1,4 +1,30 @@
 package org.itp.engine.useraccount;
 
+import org.itp.engine.linuxos.LinuxTerminal;
+import org.itp.engine.linuxos.TerminalOperationSession;
+import org.itp.engine.linuxos.TerminalOperationSessionFactory;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserAccountService {
+
+    public boolean createUser(UserAccount user){
+        TerminalOperationSession session = TerminalOperationSessionFactory
+                .getSessionWith("adduser "+ user.getLinuxUsername());
+        session
+                .addUserInput(user.getLinuxPassword())
+                .addUserInput(user.getLinuxPassword())
+                .addUserInput("")
+                .addUserInput("")
+                .addUserInput("")
+                .addUserInput("")
+                .addUserInput("")
+                .addUserInput("Y");
+
+        LinuxTerminal terminal = new LinuxTerminal();
+        terminal.setSession(session);
+        String output = terminal.executeSession();
+
+        return output.contains("Adding user");
+    }
 }

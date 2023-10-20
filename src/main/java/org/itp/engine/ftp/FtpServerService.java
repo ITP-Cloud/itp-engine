856d563019bb87603a4cdd12ad81f585bdcb6b55
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-20 15:07:25 +0300

    update: added more security and permission commands

--- src/main/java/org/itp/engine/ftp/FtpServerService.java
+++ src/main/java/org/itp/engine/ftp/FtpServerService.java
@@ -11,21 +11,46 @@ public class FtpServerService {
 
     public boolean createFtpAccount(UserAccount user){
         TerminalOperationSession session = TerminalOperationSessionFactory
-                .getSessionWith("systemctl restart vsftpd");
+                .getSessionWith("bash");
 
         session
-                .addUserInput("mkdir /home/"+user.getFtpUsername()+"/ftp")
-                .addUserInput("chown nobody:nogroup /home/"+user.getFtpUsername()+"/ftp")
-                .addUserInput("chmod a-w /home/"+user.getFtpUsername()+"/ftp")
-                .addUserInput("mkdir /home/"+user.getFtpUsername()+"/ftp/websites")
-                .addUserInput("chown "+user.getFtpUsername()+":"+user.getFtpUsername()+" /home/"+user.getFtpUsername()+"/ftp/files")
+                // Step 1: Create ftp directory
+                .addUserInput("mkdir /home/" + user.getFtpUsername() + "/ftp")
+                // Step 2: Set ownership, I also don't know why, but for security stuff I guess
+                .addUserInput("chown nobody:nogroup /home/" + user.getFtpUsername() + "/ftp")
+                .addUserInput("chmod a-w /home/" + user.getFtpUsername() + "/ftp")
+                // Step 3: Create the websites directory
+                .addUserInput("mkdir /home/" + user.getFtpUsername() + "/ftp/websites")
+                // Step 4: Create a shared group, for the new user and www-data aka apache2
+                .addUserInput("groupadd " + user.getFtpUsername() + "_n_www-data")
+                // Step 5: Add apache2 to the new shared group
+                .addUserInput("usermod -aG " + user.getFtpUsername() + "_n_www-data www-data")
+                // Step 6: Add user to the new shared group
+                .addUserInput("usermod -aG " + user.getFtpUsername() + "_n_www-data " + user.getFtpUsername())
+                // Step 7: Change ownership of the user's website directory, more security stuff
+                // The commands below supplement the vsftp configurations. They allow the user to upload
+                // files with group permissions applied already. This makes it possible for
+                // apache to scan the folder respond to requests.
+
+                .addUserInput("chown -R www-data:" + user.getFtpUsername() + "_n_www-data /home/" + user.getFtpUsername())
+                .addUserInput("chmod 770 /home/" + user.getFtpUsername() + "/ftp/websites")
+                .addUserInput("chgrp -R " + user.getFtpUsername() + "_n_www-data /home/" + user.getFtpUsername() + "/ftp/websites")
+                .addUserInput("chmod -R g+w /home/" + user.getFtpUsername() + "/ftp/websites")
+                .addUserInput("find /home/" + user.getFtpUsername() + "/ftp/websites -type d -exec chmod 2770 {} \\;")
+                .addUserInput("find /home/" + user.getFtpUsername() + "/ftp/websites -type f -exec chmod ug+rw {} \\;")
+
+                // Allow the group to write to the websites, this is primarily for the user, since the apache2 user already has access
+                .addUserInput("chmod g+w -R /home/" + user.getFtpUsername() + "/ftp/websites")
+                // Step 8: Grant user ftp access
                 .addUserInput("echo \""+user.getFtpUsername()+"\" | sudo tee -a /etc/vsftpd.userlist")
+                // Step 9: Limit user terminal access
                 .addUserInput("usermod "+user.getFtpUsername()+" -s /bin/ftponly")
+                // Step 10: Cleanup
                 .addUserInput("systemctl restart vsftpd");
 
         LinuxTerminal terminal = new LinuxTerminal();
         terminal.setSession(session);
-        terminal.executeSession();
+        System.out.println(terminal.executeSession());
 
         return true;
     }

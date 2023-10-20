a79bb3e635bff7c063daf654cf98cebd5779a05e
Aaron Mkandawire <77462566+AaronMk44@users.noreply.github.com>
2023-10-20 15:13:44 +0300

    update: implemented endpoint to provision website

--- src/main/java/org/itp/engine/website/WebsiteController.java
+++ src/main/java/org/itp/engine/website/WebsiteController.java
@@ -1,4 +1,33 @@
 package org.itp.engine.website;
 
+import org.itp.engine.restful.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/")
 public class WebsiteController {
+
+    private final WebsiteService service;
+
+    @Autowired
+    public WebsiteController(WebsiteService service) {
+        this.service = service;
+    }
+
+    @PostMapping("website/new")
+    public ResponseEntity<ApiResponse> createWebsite(@RequestBody Website website) {
+
+        this.service.createWebsite(website);
+
+        ApiResponse response = new ApiResponse(
+                "Website successfully created",
+                HttpStatus.OK.value()
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
